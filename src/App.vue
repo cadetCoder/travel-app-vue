723182b038d217ec3219bd1604707de205de63fd
Darrel Toledo <darreltoledo@icloud.com>
2021-05-22 22:23:01 +0100

    router link to Brazil page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div id="nav">
+      <router-link to="/">Home</router-link>
+      <router-link to="/brazil">Brazil</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
 
@@ -11,7 +14,7 @@ import HelloWorld from "./components/HelloWorld.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Brazil,
   },
 };
 </script>
