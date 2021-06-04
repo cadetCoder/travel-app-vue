4df87a7328f3642691c005e39a03b6cbf7408746
Darrel Toledo <darreltoledo@icloud.com>
2021-06-04 21:39:34 +0100

    experience details implemented with image

--- src/views/ExperienceDetails.vue
+++ src/views/ExperienceDetails.vue
@@ -2,5 +2,11 @@
   <section>
     <h1>Experiences</h1>
     <h2>{{ destination.name }}</h2>
+    <div class="experience-details">
+      <img
+        :src="require(`@/assets/${experience.image}`)"
+        :alt="experience.name"
+      />
+    </div>
   </section>
 </template>
