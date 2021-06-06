8e33dfa0d71fc797831a401eb4c64c99485307b6
Darrel Toledo <darreltoledo@icloud.com>
2021-06-06 18:42:13 +0100

    Go back

---
+++ src/components/GoBack.vue
@@ -0,0 +1,14 @@
+<template>
+  <span class="go-back">
+    <button @click="goBack"><i class="fas fa-chevron-left"></i>Go back</button>
+  </span>
+</template>
+<script>
+export default {
+  methods: {
+    goBack() {
+      return this.$router.go(-1);
+    },
+  },
+};
+</script>
