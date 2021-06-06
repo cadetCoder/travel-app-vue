4ba8bd4bfe78b4914d295c125e2a98eed18c4788
Darrel Toledo <darreltoledo@icloud.com>
2021-06-06 22:28:54 +0100

    route transitions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <TheNavigation />
-    <router-view :key="$route.path" />
+    <transition name="slide" mode="out-in">
+      <router-view :key="$route.path" />
+    </transition>
   </div>
 </template>
 <script>
