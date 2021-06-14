3f1ea36f06005aaf9d72fdf87ce612c2cbf4d8ad
Darrel Toledo <darreltoledo@icloud.com>
2021-06-14 22:09:55 +0100

    user page

---
+++ src/views/User.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Welcome {{ user }}</h1>
+    <p>
+      <router-link
+        :to="{
+          name: 'invoices',
+        }"
+      >
+        Invoices
+      </router-link>
+    </p>
+  </div>
+</template>
