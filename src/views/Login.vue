c148ea02a9b6f71ad0a816dd178e4d866a6c0f12
Darrel Toledo <darreltoledo@icloud.com>
2021-06-15 14:12:29 +0100

    login form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="login">
     <h1>Login</h1>
-    <div class="form"></div>
+    <div class="form">
+      <label for="username">Username</label>
+      <input type="text" name="username" class="input" />
+      <label for="password">Password</label>
+      <input type="password" class="input" />
+      <button class="btn">Login</button>
+    </div>
   </div>
 </template>
