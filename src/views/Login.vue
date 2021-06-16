417d67f71dbf4177fbac5eb265c2735d1e898469
Darrel Toledo <darreltoledo@icloud.com>
2021-06-16 22:17:13 +0100

    v-model directives

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,10 +3,20 @@
     <h1>Login</h1>
     <div class="form">
       <label for="username">Username</label>
-      <input type="text" name="username" class="input" />
+      <input v-model="username" type="text" name="username" class="input" />
       <label for="password">Password</label>
-      <input type="password" class="input" />
+      <input v-model="password" type="password" class="input" />
       <button class="btn">Login</button>
     </div>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      username: null,
+      password: null,
+    };
+  },
+};
+</script>
