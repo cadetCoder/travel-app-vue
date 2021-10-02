016d1a2d809fb387b38ae79abe77bfb8f5bc6ad8
Darrel Toledo <darreltoledo@icloud.com>
2021-10-02 18:39:50 +0100

    styled footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,9 @@
 <template>
-  <footer><p>Hello footer</p></footer>
+  <footer>
+    <div class="footer">
+      <p>Hello footer</p>
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -12,4 +16,14 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: #42b983;
+  color: white;
+  text-align: center;
+}
+</style>
